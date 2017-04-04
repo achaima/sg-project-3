5c1f43f25368b9a3eda4135d323cbad8ea721199
Asma Chaima <achaima@spartaglobal.co>
2017-04-04 14:31:28 +0100

    basic setup for project

---
+++ config/router.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+
+// TODO: fill in your router as required
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var router = require('./config/router');
+var app = express();
+var PORT = process.env.PORT || 3000;
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/sg-webdev4-project3';
+
+mongoose.connect(MONGODB_URI, function (err) {
+  if (err) {
+    console.error('Could not connect to Mongo: err:', err);
+    process.exit(1);
+  }
+  console.log('Connected to database:', mongoose.connection.name);
+});
+
+app.use(morgan('dev'));
+app.use(express.static('node_modules'));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(PORT, function() {
+  console.log('App is running on port', PORT);
+});
+
+module.exports = app;
