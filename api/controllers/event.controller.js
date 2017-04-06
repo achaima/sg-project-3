a863c030769465e937e33533f9ee2a22498c98cd
Asma Chaima <achaima@spartaglobal.co>
2017-04-06 12:02:46 +0100

    implementing the API to get external data

--- api/controllers/event.controller.js
+++ api/controllers/event.controller.js
@@ -5,12 +5,22 @@ console.log('event.controller: eventful:', eventful);
 
 function findEvents(req, res) {
   var location = req.query.location;
-  console.log('findEvents(): location:', location);
-  // TODO: here is where we will call the external API instead of returning hard-coded data:
-  res.json([
-    { name: 'The Holi Festival' },
-    { name: 'Mo\'s Comedy Show' }
-  ]);
+  // sample URL to search by location:
+  // http://api.eventful.com/rest/events/search?app_key=YOUR_API_KEY&location=London&date=Future
+  var url = `${eventful.EVENTFUL_BASE_URL}/events/search?app_key=${eventful.EVENTFUL_API_KEY}&location=${location}&date=Future`;
+
+  request(url, (error, response, body) => {
+    var eventsJson;
+
+    if (error) {
+      console.warn('findEvents: could not get events:', error);
+      res.status(500).json({message: 'could not get events'});
+      return;
+    }
+    eventsJson = JSON.parse(body);
+    console.log('eventsSearchResults:', eventsJson);
+    res.status(200).json(eventsJson.events.event);
+  });
 }
 
 module.exports = {
