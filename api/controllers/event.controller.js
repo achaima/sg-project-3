ee07d7756654c74aee0c2a7be1f16b3b5ab7ec65
Asma Chaima <achaima@spartaglobal.co>
2017-04-11 10:02:18 +0100

    restructure the search result boxes

--- api/controllers/event.controller.js
+++ api/controllers/event.controller.js
@@ -8,7 +8,7 @@ function findEvents(req, res) {
   var date = req.query.date;
   // sample URL to search by location:
   // http://api.eventful.com/rest/events/search?app_key=YOUR_API_KEY&location=London&date=Future
-  var url = `${eventful.EVENTFUL_BASE_URL}/events/search?app_key=${eventful.EVENTFUL_API_KEY}&location=${location}&date=${date}&image_sizes=block200&page_size=80&include=popularity`;
+  var url = `${eventful.EVENTFUL_BASE_URL}/events/search?app_key=${eventful.EVENTFUL_API_KEY}&location=${location}&date=${date}&image_sizes=block200&page_size=40&include=popularity`;
 
   request(url, (error, response, body) => {
     var eventsJson;
